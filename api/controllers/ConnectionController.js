f127e7549943a6da83bd57b79ea8d5d6d9b0db82
Valentino Houessou <valentino.houessou@gmail.com>
2015-12-28 22:59:29 +0100

    parsing data

--- api/controllers/ConnectionController.js
+++ api/controllers/ConnectionController.js
@@ -7,15 +7,18 @@
 
 module.exports = {
 
-    getData: function(req, res){
-        var net = require('net');
+    getData: function (req, res) {
+        var net, HOST, PORT, client, input,
+            splitRes1, splitRes2, login, ip, salle, promo;
+        
+        net = require('net');
 
-        var HOST = 'ns-server.epita.fr';
-        var PORT = 4242;
+        HOST = 'ns-server.epita.fr';
+        PORT = 4242;
 
-        var client = new net.Socket();
-        client.connect(PORT, HOST, function() {
-
+        client = new net.Socket();
+        client.connect(PORT, HOST, function () {
+            
             console.log('CONNECTED TO: ' + HOST + ':' + PORT);
             // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
             client.write('list_users\n');
@@ -23,14 +26,39 @@ module.exports = {
 
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
-        var input = '';
-        client.on('data', function(data) {
+        
+        input = '';
+        splitRes1 =  [];
+        splitRes2 =  [];
+        login = '';
+        ip = '';
+        salle = '';
+        promo = '';
+        
+        client.on('data', function (data) {
 
             input = input + data + '\n';
-            console.log('DATA: ' + data);
+            //console.log('DATA: ' + data /*+ '///'*/);
+            
+            //Split of data
+            splitRes1 = data.toString().split("\n");
+            
+            splitRes1.forEach(function (data, index) {
+                
+                splitRes2 = data.toString().split(" ");
+                if ((splitRes2[0] !== "") && (splitRes2[2] !== undefined)
+                        && (splitRes2[2].indexOf(".") !== -1)) {
+
+                    login = splitRes2[1];
+                    ip = splitRes2[2];
+                    salle = splitRes2[2];
+                    promo = splitRes2[9];
+                    console.log(login + " / " + ip + " / " + salle + " / " + promo);
+                }
+            });
+            
             // Close the client socket completely
-            if ( data.indexOf("rep 002 -- cmd end\n") != -1)
-            {
+            if (data.indexOf("rep 002 -- cmd end\n") !== -1) {
                 client.destroy();
                 res.status(200).send(input);
             }
@@ -38,7 +66,7 @@ module.exports = {
         });
 
 // Add a 'close' event handler for the client socket
-        client.on('close', function() {
+        client.on('close', function () {
             console.log('Connection closed');
         });
 
