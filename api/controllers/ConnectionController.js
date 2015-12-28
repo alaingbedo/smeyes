927fef48982052c7d5b52145f9f51d34dc6a3883
Kevin Gosse <gossekevin1@gmail.com>
2015-12-28 17:04:40 +0100

    retrieve the data

--- api/controllers/ConnectionController.js
+++ api/controllers/ConnectionController.js
@@ -6,6 +6,42 @@
  */
 
 module.exports = {
-	
+
+    getData: function(req, res){
+        var net = require('net');
+
+        var HOST = 'ns-server.epita.fr';
+        var PORT = 4242;
+
+        var client = new net.Socket();
+        client.connect(PORT, HOST, function() {
+
+            console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+            // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+            client.write('list_users\n');
+        });
+
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
+        var input = '';
+        client.on('data', function(data) {
+
+            input = input + data + '\n';
+            console.log('DATA: ' + data);
+            // Close the client socket completely
+            if ( data.indexOf("rep 002 -- cmd end\n") != -1)
+            {
+                client.destroy();
+                res.status(200).send(input);
+            }
+
+        });
+
+// Add a 'close' event handler for the client socket
+        client.on('close', function() {
+            console.log('Connection closed');
+        });
+
+    }
 };
 
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,9 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+
+  'get /data': 'ConnectionController.getData'
 
   /***************************************************************************
   *                                                                          *
