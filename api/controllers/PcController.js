c6a8f09582c31f956e672b5fa4ba3041b6676802
Valentino Houessou <valentino.houessou@gmail.com>
2015-12-28 14:48:21 +0100

    first api

---
+++ api/controllers/PcController.js
@@ -0,0 +1,11 @@
+/**
+ * PcController
+ *
+ * @description :: Server-side logic for managing pcs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
