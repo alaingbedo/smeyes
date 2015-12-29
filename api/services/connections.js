2f56615b41ae18f917c4b4f2eafa4e77956dffa4
Valentino Houessou <valentino.houessou@gmail.com>
2015-12-29 10:04:33 +0100

    services connections update

--- api/services/connections.js
+++ api/services/connections.js
@@ -1,48 +1,86 @@
 /**
  * Created by kevin gosse on 29/12/2015.
  */
-/*var net = require('net');
+var net = require('net');
 
+/*
+@description :: delete duplicate connection in the collection's array
+*/
 function deleteDuplicatedData(data) {
-
+    
 }
 
+/*
+@description :: Parsing the data get from the soulnet server
+*/
 function parsing(data){
-    var splitRes1 =  [];
-    var splitRes2 =  [];
+    
+    var splitRes1, splitRes2,
+            connection, connectionsTable;
+    
+    splitRes1 =  [];
+    splitRes2 =  [];
+    connectionsTable = [];
+    connection = {};
+    
+    splitRes1 = data.toString().split("\n");
+
+    splitRes1.forEach(function (data, index) {
+
+        splitRes2 = data.toString().split(" ");
+        if ((splitRes2[0] !== "") && (splitRes2[2] !== undefined)
+            && (splitRes2[2].indexOf(".") !== -1)) {
+
+            connection = {
+                username : splitRes2[1],
+                promo : splitRes2[9],
+                ip : splitRes2[2],
+                start : new Date().getTime()
+            };
+            connectionsTable.push(connection);
+            connection = {};    
+        } 
+    
+   });
+
+   return connectionsTable;
 }
 
+/*
+@description :: getting the data get from the soulnet server
+*/
 function getData(){
-    var HOST, PORT, client, input,
-        splitRes1, splitRes2, login, ip, salle, promo,
-        connection, connections;
 
-
+    var HOST, PORT, client, input, connectionsTable;
+    
     HOST = 'ns-server.epita.fr';
     PORT = 4242;
 
     client = new net.Socket();
     client.connect(PORT, HOST, function () {
+
+        console.log('CONNECTED TO: ' + HOST + ':' + PORT);
         client.write('list_users\n');
+
     });
 
-    input = '';
     client.on('data', function (data) {
 
-        input = input + data + '\n';
+        //Split of data
+        connectionsTable = parsing(data);
 
         // Close the client socket completely
         if (data.indexOf("rep 002 -- cmd end\n") !== -1) {
             client.destroy();
-            return deleteDuplicatedData(parsing(input));
+            return connectionsTable;
         }
 
     });
-
+    
     client.on('close', function () {
         console.log('Connection closed');
     });
-
+    
 }
 
-module.exports = getData;*/
+module.exports = getData;
